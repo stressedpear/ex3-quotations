ff37c010b51fc8ac5487a6405778449a647d1f24
stressedpear <jocelyn0santos3@gmail.com>
2023-01-31 15:28:19 -0500

    Created quote app

---
+++ index.js
@@ -0,0 +1,9 @@
+const quotes = [
+    "You do not have to be good",
+    "You only have to set the soft animal of your body love what it loves",
+    "the world offers itself to your imagination"
+]
+
+let randomNumber = Math.floor(Math.random() * (quotes.length));
+
+console.log(quotes[randomNumber])
